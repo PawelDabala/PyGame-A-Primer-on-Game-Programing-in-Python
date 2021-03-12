5628e7ad8cf4a7fa21c7652cbde147e981666ca2
Pawel Dabala <pdabala@gmial.com>
2021-03-12 13:10:39 +0100

    set constans keys

---
+++ crush_game.py
@@ -0,0 +1,20 @@
+import pygame
+
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT
+)
+
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+#Create the screen object
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
